ebde30d8dc960abc00241b581c4553ba14599297
lc3267 <lc3267@kit.edu>
2026-01-29 14:42:15 +0000

    script for sweeping variances
    Signed-off-by: lc3267 <lc3267@kit.edu>

--- scripts/sweep_seeds.sh
+++ scripts/sweep_seeds.sh
@@ -5,17 +5,20 @@ set -e
 # run experiments with all different circuits
 MAX_JOBS=3
 
-for seed in 1000 1001 1002 1003 1004 1005 1006 1007 1008 1009
+for variance in 0.015 0.016 0.017 0.018 0.019
 do
-    # Wait if we already have MAX_JOBS running
-    while [ $(jobs -r | wc -l) -ge $MAX_JOBS ]; do
-        sleep 5
+    for seed in 1000 1001 1002 1003 1004 1005 1006 1007 1008 1009
+    do
+        # Wait if we already have MAX_JOBS running
+        while [ $(jobs -r | wc -l) -ge $MAX_JOBS ]; do
+            sleep 5
+        done
+
+        echo "--- Variance $variance, Seed $seed ---"
+        uv run kedro run --params="fcc.seed=$seed,fcc.pulse_params_variance=$variance" &
+
+        sleep 30
     done
-
-    echo "--- Seed $seed ---"
-    uv run kedro run --params="fcc.seed=$seed" &
-
-    sleep 30
 done
 
 # Wait for all remaining jobs to complete
