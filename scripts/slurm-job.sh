34a18604aebdeb4c6a3558f9047d893fed8193af
lc3267 <lc3267@kit.edu>
2026-02-02 09:53:42 +0100

    slurm setup
    Signed-off-by: lc3267 <lc3267@kit.edu>

---
+++ scripts/slurm-job.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# 
+# name of the job for better recognizing it in the queue overview
+#SBATCH --job-name=pulse-level-qfms
+# 
+# define how many nodes we need
+#SBATCH --nodes=1
+#
+# we only need on 1 cpu at a time
+#SBATCH --ntasks=20
+#
+# expected duration of the job
+#              hh:mm:ss
+#SBATCH --time=10:00:00
+# 
+# partition the job will run on
+#SBATCH --partition cpu
+# 
+# expected memory requirements
+#SBATCH --mem=30000MB
+#
+# infos
+#
+# output path
+#SBATCH --output="logs/slurm/slurm-%j-%x.out"
+
+module load compiler/llvm
+module load devel/python/3.11.7
+
+cd ~/pulse-level-quantum-fourier-models
+source ./scripts/study-1.sh
--- scripts/study-1.sh
+++ scripts/study-1.sh
@@ -3,13 +3,13 @@
 set -e
 
 # run experiments with all different circuits
-MAX_JOBS=5
+MAX_JOBS=20
 
 # Hardware_Efficient Circuit_15 Circuit_17 Circuit_19
 for circuit in Hardware_Efficient Circuit_15 Circuit_17 Circuit_19
 do
     # 0.001 0.002 0.003 0.004 0.005 0.006 0.007 0.008 0.009 0.010 0.011 0.012 0.013 0.014 0.015 0.016 0.017 0.018 0.019 0.020
-    for variance in 0.001 0.002 0.003 0.004 0.005 0.006 0.007 0.008 0.009 0.010 0.011 0.012 0.013 0.014 0.015 0.016 0.017 0.018 0.019 0.020
+    for variance in 0.0 0.001 0.002 0.003 0.004 0.005 0.006 0.007 0.008 0.009 0.010
     do
         # 1000 1001 1002 1003 1004 1005 1006 1007 1008 1009
         for seed in 1000 1001 1002 1003 1004 1005 1006 1007 1008 1009
