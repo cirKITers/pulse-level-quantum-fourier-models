f38e135b1a115d882dab194d8d058396e572ccad
lc3267 <lc3267@kit.edu>
2026-03-26 16:13:00 +0000

    fixed data gen
    Signed-off-by: lc3267 <lc3267@kit.edu>

--- src/pulse_level_qfms/pipelines/generation/nodes.py
+++ src/pulse_level_qfms/pipelines/generation/nodes.py
@@ -1,8 +1,9 @@
 from qml_essentials.model import Model
-from qml_essentials.ansaetze import Gates
 from qml_essentials.coefficients import Datasets
 
 from typing import List, Dict, Union, Callable
+import jax
+import jax.numpy as jnp
 import numpy as np
 import mlflow
 
@@ -13,7 +14,8 @@ import logging
 
 log = logging.getLogger(__name__)
 
-from pulse_level_qfms.utils import Sampling
+
+jax.config.update("jax_enable_x64", True)
 
 
 def generate_model(
@@ -55,26 +57,24 @@ def generate_fourier_series(
     coefficients_max: float,
     zero_centered: bool,
     seed: int,
-) -> np.ndarray:
+) -> jnp.ndarray:
     """
     Generates the Fourier series representation of a function.
 
     Parameters
     ----------
-    domain_samples : np.ndarray
+    domain_samples : jnp.ndarray
         Grid of domain samples.
     omega : List[List[float]]
         List of frequencies for each dimension.
 
     Returns
     -------
-    np.ndarray
+    jnp.ndarray
         Fourier series representation of the function.
     """
-    rng = np.random.default_rng(seed)
-
     domain_samples, fourier_samples, coefficients = Datasets.generate_fourier_series(
-        rng=rng,
+        random_key=jax.random.PRNGKey(seed),
         model=model,
         coefficients_min=coefficients_min,
         coefficients_max=coefficients_max,
@@ -89,15 +89,15 @@ def generate_fourier_series(
 
 
 def build_fourier_series_dataloader(
-    batch_size: int, domain_samples, fourier_samples, coefficients: np.ndarray
+    batch_size: int, domain_samples, fourier_samples, coefficients: jnp.ndarray
 ):
     if batch_size < 1:
         batch_size = domain_samples.shape[0]
     train_loader = DataLoader(
         TensorDataset(
-            torch.from_numpy(domain_samples),
-            torch.from_numpy(fourier_samples).squeeze(),
-            torch.from_numpy(coefficients).squeeze(),
+            torch.from_numpy(np.array(domain_samples)),
+            torch.from_numpy(np.array(fourier_samples).squeeze()),
+            torch.from_numpy(np.array(coefficients).squeeze()),
         ),
         batch_size=batch_size,
         shuffle=False,
