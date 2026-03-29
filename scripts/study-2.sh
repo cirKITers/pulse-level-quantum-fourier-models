4cd99e6ed1598010c70c26335ce014b1226f460b
lc3267 <lc3267@kit.edu>
2026-03-29 09:01:51 +0000

    simplified
    Signed-off-by: lc3267 <lc3267@kit.edu>

--- scripts/study-2.sh
+++ scripts/study-2.sh
@@ -12,8 +12,7 @@ MAX_JOBS=20
 # All circuits
 for circuit in Circuit_2 Circuit_4 Circuit_8 Circuit_14 Circuit_15 Circuit_17 Circuit_19 Circuit_20 Strongly_Entangling Circuit_3 Circuit_9 Circuit_10 Circuit_16 Circuit_18 Circuit_7 Circuit_13 Hardware_Efficient 
 do
-    # 0.0 0.001 0.002 0.003 0.004 0.005 0.006 0.007 0.008 0.009 0.010 0.011 0.012 0.013 0.014 0.015 0.016 0.017 0.018 0.019 0.020
-    for variance in 0.0 0.001 0.002 0.003 0.004 0.005 0.006 0.007 0.008 0.009 0.010 0.012 0.014
+    for variance in 0.0 0.001 0.002 0.003 0.004 0.005 0.006 0.007 0.008
     do
         # 1000 1001 1002 1003 1004 1005 1006 1007 1008 1009
         for seed in 1000 1001 1002 1003 1004 1005 1006 1007 1008 1009
