8bbe7d7637e2b4487f1bf7f00f59be02b6d0a01f
lc3267 <lc3267@kit.edu>
2026-04-08 15:43:19 +0200

    log axes
    Signed-off-by: lc3267 <lc3267@kit.edu>

--- notebooks/viz_helper.py
+++ notebooks/viz_helper.py
@@ -591,6 +591,8 @@ def fcc_over_distortion(df: pd.DataFrame, max_distortion, show_error):
             line=dict(color=next(color_it)),
         )
 
+    fig.update_yaxes(type="log")
+
     fig.update_layout(
         title="FCC over PP Variances",
         xaxis_title="Pulse Parameter Variances",
